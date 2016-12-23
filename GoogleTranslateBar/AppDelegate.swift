a63efa1fc6f25793f5b5fe380dcb4fddf562d818
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-12-23 14:53:34 +0100

    init

---
+++ GoogleTranslateBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GoogleTranslateBar
+//
+//  Created by Vadim Dez on 23/12/2016.
+//  Copyright © 2016 Vadim Dez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
