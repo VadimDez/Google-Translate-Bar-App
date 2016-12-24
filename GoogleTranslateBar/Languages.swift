2c91d5cdfb275f89a42b505fdd58437ffe52d695
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-12-24 02:13:43 +0100

    further

---
+++ GoogleTranslateBar/Languages.swift
@@ -0,0 +1,15 @@
+//
+//  Languages.swift
+//  GoogleTranslateBar
+//
+//  Created by Vadim Dez on 24/12/2016.
+//  Copyright © 2016 Vadim Dez. All rights reserved.
+//
+
+import Foundation
+
+let languages = [
+    "Russian": "ru",
+    "English": "en",
+    "German": "de"
+]
--- GoogleTranslateBar/MainViewController.swift
+++ GoogleTranslateBar/MainViewController.swift
@@ -10,9 +10,39 @@ import Cocoa
 
 class MainViewController: NSViewController {
 
+    @IBOutlet weak var languagesFrom: NSPopUpButton!
+    @IBOutlet weak var languagesTo: NSPopUpButton!
+    @IBOutlet weak var translateButton: NSButton!
+    @IBOutlet weak var textFieldToTranslate: NSTextField!
+    @IBOutlet weak var progressLabel: NSTextField!
+    @IBOutlet weak var translatedTextField: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        self.progressLabel.stringValue = ""
+        
+        self.setLanguages()
     }
     
+    func setLanguages() -> Void {
+        self.languagesFrom.removeAllItems()
+        self.languagesTo.removeAllItems()
+        
+        languages.forEach { (name, value) in
+            self.languagesFrom.addItem(withTitle: name)
+            self.languagesTo.addItem(withTitle: name)
+        }
+        
+        self.languagesFrom.select(self.languagesFrom.item(at: 1))
+        self.languagesTo.select(self.languagesTo.item(at: 0))
+    }
+    
+    @IBAction func onTranslate(_ sender: Any) {
+        self.progressLabel.stringValue = "Translating..."
+        
+        self.translatedTextField.stringValue = self.textFieldToTranslate.stringValue
+        
+        self.progressLabel.stringValue = ""
+    }
 }
